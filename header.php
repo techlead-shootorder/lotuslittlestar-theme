0ab8c323f96e089f640f79f5f774ab315626dbf8
rohan@shootorder <rohan@shootorder.in>
2026-06-30 17:46:36 +0530

    updated special card section

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 	<link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Mulish:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
 	<!-- Tailwind Play CDN -->
-	<script src="https://cdn.tailwindcss.com"></script>
+	<script src="https://cdn.tailwindcss.com?plugins=typography"></script>
 	<script>
 		tailwind.config = {
 			theme: {
